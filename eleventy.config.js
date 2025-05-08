ad6ae1f0d427dbb508cc12b60ebcdf36abddbaa3
Wouter Damen <damen.wouter@gmail.com>
2025-05-08 15:07:28 +0200

    Initial commit

---
+++ eleventy.config.js
@@ -0,0 +1,37 @@
+import htmlmin from "html-minifier-terser";
+import CleanCSS from "clean-css";
+
+export default function (eleventyConfig) {
+  eleventyConfig.setInputDirectory("src");
+  eleventyConfig.setOutputDirectory("build");
+  eleventyConfig.setIncludesDirectory("partials");
+
+  eleventyConfig.addPassthroughCopy("src/img");
+  eleventyConfig.addPassthroughCopy("src/js");
+
+  eleventyConfig.setTemplateFormats(["liquid", "css", "html"]);
+
+  eleventyConfig.addExtension("css", {
+    outputFileExtension: "css",
+    compile: async function (inputContent) {
+      return async () => new CleanCSS({}).minify(inputContent).styles;
+    },
+  });
+
+  eleventyConfig.setLiquidOptions({
+    dynamicPartials: true,
+    strictFilters: false, // renamed from `strict_filters` in Eleventy 1.0
+  });
+
+  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+    if (outputPath && outputPath.endsWith(".html")) {
+      return htmlmin.minify(content, {
+        collapseWhitespace: true,
+        removeComments: true,
+        minifyCSS: true,
+        minifyJS: true,
+      });
+    }
+    return content;
+  });
+}
---
+++ src/js/main.js
@@ -0,0 +1,31 @@
+document.addEventListener("readystatechange", () => {
+  // Mobile nav toggle
+  document.querySelector(".mobile-nav-btn").addEventListener("click", () => {
+    const ul = document.querySelector("nav ul");
+    if (ul.classList.contains("open")) {
+      ul.classList.remove("open");
+      ul.classList.remove("opened");
+    } else {
+      ul.classList.add("open");
+      setTimeout(() => {
+        ul.classList.add("opened");
+      }, 100);
+    }
+  });
+
+  // Language switch toggle
+  document
+    .querySelector(".language-switch-btn")
+    .addEventListener("click", (e) => {
+      const menu = document.querySelector("nav .language-switch");
+      if (menu.classList.contains("open")) {
+        menu.classList.remove("open");
+        menu.classList.remove("opened");
+      } else {
+        menu.classList.add("open");
+        setTimeout(() => {
+          menu.classList.add("opened");
+        }, 100);
+      }
+    });
+});
